31c0eec7b225f8cd9f66aaf7febb5b7f81086e05
unifino <unifino@protonmail.com>
2024-07-09 00:29:04 +0330

    LogIn

--- src/Departments/DataBase/db.ts
+++ src/Departments/DataBase/db.ts
@@ -21,7 +21,22 @@ export const getPatients = () => {
     return new Promise ( (rs, rx) => {
         const qry = `SELECT * FROM Patients`;
         // WHERE email = '${email}'
-        db.query( qry, ( err, r: Result ) => err ? rx(err) : rs( r.rows ) );
+        db.query( qry, ( err, r: Result ) => err ? rx( "E001 " + err ) : rs( r.rows ) );
+    } );
+}
+
+// -- ======================================================================================
+
+export const logIn = ( username: string, password: string ) => {
+    return new Promise ( (rs, rx) => {
+        // ! Consider it
+        const qry = `SELECT * FROM Patients WHERE username = '${username}' AND password = '${password}'`
+        db.query( qry, ( err, r: Result ) => {
+            if ( err ) rx( "E001 " + err )
+            else if ( r.rows.length === 0 ) rx( "User Not Found" )
+            else if ( r.rows.length === 1 ) rs( r.rows[0] )
+            else rx( "E002 : Duplicated Users!" )
+        } )
     } );
 }
 
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import * as express                     from "express"
 import * as TS                          from './types/types'
 import * as CTS                         from './types/common'
 import * as db                          from './Departments/DataBase/db'
+import { error } from "console";
 
 // -- ========================================================================= INIT =======
 
@@ -33,9 +34,29 @@ app.get( '/getPatients', ( req: express.Request, res: express.Response ) => {
     // .. Process Request by DB Department
     db.getPatients()
     // .. everything is good
-    .then( patients => res.json( { status: 200, answer: patients } ) )
+    .then( patients => res.json( { status: 200, scc: patients } ) )
     // .. report error
-    .catch( err => res.json( { status: 500, reason: err + "" } ) );
+    .catch( err => res.json( { status: 500, err: err + "" } ) );
+
+} );
+
+// -- ============================================================ Get Patients List =======
+
+app.post( '/logIn', async ( req: express.Request, res: express.Response ) => {
+
+    // .. requests | assign requests from POST
+    const username = req.body.username as string;
+    const password = req.body.password as string;
+    
+    // ! remove it
+    await new Promise( _ => setTimeout( _, 1000 ) )
+
+    // .. Process Request by DB Department
+    db.logIn( username, password )
+    // .. everything is good
+    .then( user => res.json( { status: 200, scc: user } ) )
+    // .. report error
+    .catch( err => res.json( { status: 500, err: err + "" } ) );
 
 } );
 
