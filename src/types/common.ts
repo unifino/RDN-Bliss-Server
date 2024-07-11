8c24971ad06ade1a6cf7d829a72634f32f41e577
unifino <unifino@protonmail.com>
2024-07-11 15:28:06 +0330

    ...

--- src/types/common.ts
+++ src/types/common.ts
@@ -26,6 +26,21 @@ export type Patients = {
     name?: string
 }
 
+export type Dietitians = {
+    id: number
+    email: string
+    username: string
+    password: string
+    firstname: string
+    lastname: string
+    age: string
+    gender: Gender
+
+    name?: string
+}
+
+export type UserData = Dietitians | Patients; 
+
 // .. declare UserType
 export enum UserTypes { "Dietitian", "Patient", null }
 
