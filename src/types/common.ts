88f77715270d3c139f1041e219d2b56a59bbd488
Unifino <unifino@protonmail.com>
2025-02-15 17:44:43 +0330

    ...

--- src/types/common.ts
+++ src/types/common.ts
@@ -16,7 +16,58 @@ export type HashedPass = { hash: string, salt: string, iterations: number }
 
 export enum Gender { null = "Prefer not to Answer", male = "Male", female = "Female" }
 
-export type Patients = {
+export enum MartialStatus { Single, Married, Divorced, Widowed }
+export enum EducationalLevel { 
+    DoctoralDegree = "Doctoral Degree",
+    MasterDegree = "Master's Degree",
+    BachelorDegree = "Bachelor's degree",
+    AssociateDegree = "Associate degree or diploma",
+    SecondaryHighSchool = "Secondary High School education",
+    PrimaryEducation = "Primary Education",
+    Uneducated = "Uneducated"
+}
+
+export enum occupation {
+    art = "Arts and entertainment",
+    business = "Business administration",
+    industry = "Industrial and manufacturing",
+    law = "Law enforcement and armed forces",
+    science = "Science and technology",
+    null = ""
+}
+
+export type Patient = {
+    id: number
+    email: string
+    username: string
+    password: string|HashedPass
+    firstname: string
+    lastname: string
+    birthday: string
+    gender: Gender
+
+    name?: string
+    userType?: UserTypes
+
+    martialStatus?: MartialStatus
+    educationalLevel?: EducationalLevel
+    occupation?: string
+
+    GI_F: {
+        [GI_Functions.BowelMovement]: string[]
+        [GI_Functions.Digestion]: string[]
+        [GI_Functions.Appetite]: string[]
+    }
+}
+
+
+export enum GI_Functions { 
+    BowelMovement = "Bowel Movement",
+    Digestion = "Digestion",
+    Appetite ="Appetite" 
+}
+
+export type Dietitian = {
     id: number
     email: string
     username: string
@@ -30,21 +81,7 @@ export type Patients = {
     userType?: UserTypes
 }
 
-export type Dietitians = {
-    id: number
-    email: string
-    username: string
-    password: string|HashedPass
-    firstname: string
-    lastname: string
-    birthday: string
-    gender: Gender
-
-    name?: string
-    userType?: UserTypes
-}
-
-export type UserData = Dietitians | Patients; 
+export type UserData = Dietitian | Patient; 
 
 // .. declare UserType
 export enum UserTypes { "Dietitian", "Patient", null }
